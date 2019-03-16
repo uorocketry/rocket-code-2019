8c3fe5d0b52e153b27fe95ddac549a981c32c3b2
Jonathan <jonathanluu122@gmail.com>
2019-03-16 15:44:11 -0400

    all sensors are in a struct

--- allStructs/allStructs.ino
+++ allStructs/allStructs.ino
@@ -1,16 +1,40 @@
 #include <Wire.h>
 #include "SparkFunMPL3115A2.h"
 #include <Adafruit_GPS.h>
-#include <stdio.h>
+#include "Yost.h"
 MPL3115A2 myPressure;
 SoftwareSerial mySerial(3, 2);
 Adafruit_GPS myGPS(&mySerial);
+
+Yost yost;
+
+    typedef struct{ //initialize struct
+    float pitch;
+    float yaw;
+    float roll;
+    }euler_angles;
+
+    typedef struct{
+    float x;
+    float y;
+    float z;
+    }accel;
+
+    typedef struct{
+    euler_angles e_orient;
+    accel r_accel;
+    }yost_imu; // user defined data type
+
+
+unsigned long previousMillis = 0;
+const long interval = 500;
 
   typedef struct { //initialize structure
       float altitude;
       float velocity; // velocity to be implemented later (a_1 - a_0) / time_inbetween_readings
       float latitude;
       float longitude; 
+      yost_imu rocket; 
   } state;
   
 void setup() {
@@ -18,6 +42,7 @@ void setup() {
   Serial.begin(9600);  // Start serial for output
   myPressure.begin(); // Get sensor online
   myGPS.begin(9600); // Get sensor online
+ yost.begin();
  
   //Configure the sensor
   myPressure.setModeAltimeter(); // Measure altitude above sea level in meters
@@ -25,15 +50,36 @@ void setup() {
 
   myPressure.setOversampleRate(7); // Set Oversample to the recommended 128
   myPressure.enableEventFlags(); // Enable all three pressure and temp event flags 
+
+  typedef struct{ //initialize struct
+    float pitch;
+    float yaw;
+    float roll;
+    }euler_angles;
+
+    typedef struct{
+    float x;
+    float y;
+    float z;
+    }accel;
+
+    typedef struct{
+    euler_angles e_orient;
+    accel r_accel;
+    }yost_imu; // user defined data type
+
+    //yost_imu rocket = {{0,0,0},{0,0,0}}; // place holder vaules until sensor is updated
    
   typedef struct { //initialize structure
       float altitude;
       float velocity; // velocity to be implemented later (a_1 - a_0) / time_inbetween_readings
       float latitude;
       float longitude; 
+      yost_imu rocket;
   } state;
-  state init_state = {myPressure.readAltitude(), 20.0}; // random velocity picked
-  state init_state = {myGPS.latitude, myGPS.longitude}; // random velocity picked
+  state init_state = {myPressure.readAltitude(), 20.0, myGPS.latitude, myGPS.longitude,{{0,0,0},{0,0,0}} }; // random velocity picked
+  //state init_state = {myGPS.latitude, myGPS.longitude}; // random velocity picked
+
   
 // function prototype
 void update_state(state *); // pointer to struct
@@ -42,6 +88,7 @@ void update_state(state *); // pointer to struct
 void loop() {
   state init_state;
   update_state(&init_state); // function call pass memory address of init_state
+
   
   Serial.print("Altitude(m):");
   Serial.print(init_state.altitude, 2);
@@ -51,10 +98,59 @@ void loop() {
   Serial.print("Longitude(Degrees):");
   Serial.print(init_state.longitude, 2); //2?
   Serial.println();
+ 
+
+  Serial.print("Pitch: ");
+  Serial.print(init_state.rocket.e_orient.pitch); 
+  Serial.println();
+
+  Serial.print("Yaw: ");
+  Serial.print(init_state.rocket.e_orient.yaw); 
+  Serial.println();
+
+  Serial.print("Roll: ");
+  Serial.print(init_state.rocket.e_orient.roll); 
+  Serial.println();
+
+  Serial.print(""); // \n
+  Serial.println();
+
+  Serial.print("x: ");
+  Serial.print(init_state.rocket.r_accel.x);
+  Serial.println();
+
+  Serial.print("y: ");
+  Serial.print(init_state.rocket.r_accel.y); 
+  Serial.println();
+
+  Serial.print("z: ");
+  Serial.print(init_state.rocket.r_accel.z); 
+  Serial.println();
+
+  Serial.print(""); // \n
+  Serial.println();
+  
 }
 
 void update_state(state *state_ptr){ // point to memory address (get contents of mem address)
+  
   state_ptr->altitude = myPressure.readAltitude(); // the altitude field in the struct that state_ptr points to is updated to current Altitude
   state_ptr->latitude = myGPS.latitude; 
   state_ptr->longitude = myGPS.longitude; 
+  
+
+  // yost imu library 
+    float *euler_orient = yost.read_orientation_euler(); // point to memory address of array
+    
+    state_ptr->rocket.e_orient.pitch = *euler_orient; // point (imu_ptr) to e_orient and access the pitch filed
+    state_ptr->rocket.e_orient.yaw = *(euler_orient + 1);
+    state_ptr->rocket.e_orient.roll = *(euler_orient + 2);
+
+    // yost imu library 
+    float *accel = yost.read_accel_filtered(); // point to memory address of array
+    
+    state_ptr->rocket.r_accel.x = *accel; // point (imu_ptr) to r_accel and access the x filed
+    state_ptr->rocket.r_accel.y = *(accel + 1);
+    state_ptr->rocket.r_accel.z = *(accel + 2);
+
 }
