bb345a19140fd256cf1d8386c79133525baabf99
Jonathan <jonathanluu122@gmail.com>
2019-03-21 21:55:04 -0400

    I'll test the code tomorrow

--- allStructs/allStructs.ino
+++ allStructs/allStructs.ino
@@ -29,6 +29,8 @@ Yost yost;
 unsigned long previousMillis = 0;
 const long interval = 500;
 
+boolean sensor_debug = false;
+
   typedef struct { //initialize structure
       float altitude;
       float velocity; // velocity to be implemented later (a_1 - a_0) / time_inbetween_readings
@@ -88,9 +90,39 @@ void update_state(state *); // pointer to struct
 void loop() {
   state init_state;
   update_state(&init_state); // function call pass memory address of init_state
+  DISPLAY_OUTPUT();
 
   
-  Serial.print("Altitude(m):");
+  
+}
+
+void update_state(state *state_ptr){ // point to memory address (get contents of mem address)
+  
+  state_ptr->altitude = myPressure.readAltitude(); // the altitude field in the struct that state_ptr points to is updated to current Altitude
+  state_ptr->latitude = myGPS.latitude; 
+  state_ptr->longitude = myGPS.longitude; 
+  
+
+  // yost imu library 
+    float *euler_orient = yost.read_orientation_euler(); // point to memory address of array
+    
+    state_ptr->rocket.e_orient.pitch = *euler_orient; // point (imu_ptr) to e_orient and access the pitch filed
+    state_ptr->rocket.e_orient.yaw = *(euler_orient + 1);
+    state_ptr->rocket.e_orient.roll = *(euler_orient + 2);
+
+    // yost imu library 
+    float *accel = yost.read_accel_filtered(); // point to memory address of array
+    
+    state_ptr->rocket.r_accel.x = *accel; // point (imu_ptr) to r_accel and access the x filed
+    state_ptr->rocket.r_accel.y = *(accel + 1);
+    state_ptr->rocket.r_accel.z = *(accel + 2);
+
+}
+
+void DISPLAY_OUTPUT(){
+  state init_state;
+  if(!sensor_debug) {
+    Serial.print("Altitude(m):");
   Serial.print(init_state.altitude, 2);
   Serial.println();
   Serial.print("Latitude(Degrees):");
@@ -129,28 +161,13 @@ void loop() {
 
   Serial.print(""); // \n
   Serial.println();
+  }
   
+  else{
+     if (Serial.available() > 0) {
+         int x = Serial.read();
+         Serial.write(x);
+        }
+
 }
-
-void update_state(state *state_ptr){ // point to memory address (get contents of mem address)
-  
-  state_ptr->altitude = myPressure.readAltitude(); // the altitude field in the struct that state_ptr points to is updated to current Altitude
-  state_ptr->latitude = myGPS.latitude; 
-  state_ptr->longitude = myGPS.longitude; 
-  
-
-  // yost imu library 
-    float *euler_orient = yost.read_orientation_euler(); // point to memory address of array
-    
-    state_ptr->rocket.e_orient.pitch = *euler_orient; // point (imu_ptr) to e_orient and access the pitch filed
-    state_ptr->rocket.e_orient.yaw = *(euler_orient + 1);
-    state_ptr->rocket.e_orient.roll = *(euler_orient + 2);
-
-    // yost imu library 
-    float *accel = yost.read_accel_filtered(); // point to memory address of array
-    
-    state_ptr->rocket.r_accel.x = *accel; // point (imu_ptr) to r_accel and access the x filed
-    state_ptr->rocket.r_accel.y = *(accel + 1);
-    state_ptr->rocket.r_accel.z = *(accel + 2);
-
 }
