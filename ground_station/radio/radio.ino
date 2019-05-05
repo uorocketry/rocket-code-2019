a9c88d06d7d60810563479e12da85e07eaf6ad48
azqlin <anthonyzqlin@gmail.com>
2019-05-05 11:43:44 -0400

    adding radio_send function

--- ground_station/radio/radio.ino
+++ ground_station/radio/radio.ino
@@ -2,12 +2,28 @@
 // Serial mySerial(0, 1);
 int ctr;
 
+// Air data rate in one byte form, max value is 255
+// Current value: 4
+#define rx_air_speed 4
+byte radio_array[rx_air_speed];
+
 void setup() {
   Serial.begin(57600);
   ctr = 0;
 }
 
+void radio_send ( byte data ) {
+  Serial.write(data);
+  delay(10);
+}
+
 void loop() {
+
+  for (int i = 0; i < rx_air_speed; i++ ){
+    radio_array[i] = i+1;
+    radio_send(radio_array[i]);
+  }
+ 
   if (Serial.available() && ctr < 20) {
     if (ctr == 0) {
         delay(500);
