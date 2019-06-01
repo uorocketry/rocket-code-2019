c0735d2386855a69f61df109514ed303b43774b3
azqlin <anthonyzqlin@gmail.com>
2019-06-01 16:09:28 -0400

    Radio 1

--- ground_station/radio.py
+++ ground_station/radio.py
@@ -2,44 +2,49 @@
 
 import serial, time, re
 
+SLEEP_INTERVAL = 2
 
-file_name = 'rocket_data.csv'
-total_len = 22
-write_len = [8, 12]
-write_len.extend([total_len-write_len[0]-write_len[1]])
-write_len_dict = {
-    'time': 8,
-    'GPS': 12
-}
-
-## BS code
-def print_line():
-    read_data = ser.readline()
-    print(read_data.decode() + ', \r\n')
-
-def gen_rand_bytes():
-    import random
-    max_val = int(str(9)*total_len)
-    rand_num = random.randint(1,max_val)
-    return bytearray(str(rand_num), 'utf-8')
-
-## Radio code
-def test_baud():
-    for test_baud in reversed(serial_speeds.keys()):
-        ser.baudrate = test_baud
-        ser.open()
-        print('Testing serial port at', test_baud, 'baud.')
-        if command_mode():
-            print('Test passed at', test_baud, 'baud.')
-            return(test_baud)   # return the baud value and leave serial port open
-        print('Test failed at', test_baud, 'baud.')
-        ser.close()
-    print('Could not determine baud rate!  Exiting.')
-    exit(100)
-
-def enter_AT_mode():
+def send_command(command):
     flush_ser()
-    exit_AT_mode('RT')
+
+    ser.write(command)
+    print('Sent command: ', command.decode())
+    time.sleep(SLEEP_INTERVAL)
+    return ser.readline()
+
+def exit_AT_mode(opt):
+    flush_ser()
+
+    command = b'\r\n'        # the ATO command must start on a newline
+    ser.write(command)
+    print('Sent command: (newline and return)', command.decode())
+    time.sleep(SLEEP_INTERVAL/2)
+    if (opt == 'RT'):
+        command = b'ATO\r\n'     # exit AT command mode if we are in it
+    else:
+        command = b'RTO\r\n'
+    ser.write(command)
+    print('Sent command:', command.decode())
+    time.sleep(SLEEP_INTERVAL/2)
+
+    return ser.readline()
+
+ser = serial.Serial(
+    port='/dev/ttyS4',
+    baudrate=57600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout = 5
+)
+
+def flush_ser():
+    ser.flushOutput()
+    ser.flushInput()
+    time.sleep(SLEEP_INTERVAL)           # give the flush a second
+
+def setup():
+    flush_ser()
 
     command = b'\r\n'        # the ATO command must start on a newline
     ser.write(command)
@@ -55,101 +60,12 @@ def enter_AT_mode():
     print('Sent command:', command.decode())
     time.sleep(3)           # minimum 1 second wait after +++
 
-    print(get_response())
-
-def flush_ser():
-    ser.flushOutput()
-    ser.flushInput()
-    time.sleep(1)           # give the flush a second
-
-def send_command(command):
-    flush_ser()
-
-    ser.write(command)
-    print('Sent command: ', command.decode())
-    time.sleep(1)
-
-    response = get_response()
-
-def get_response():
-    sleep_time_after_buffer_read = 2
-    inBuffer = ser.inWaiting()
-    print("Characters in receive buffer before reading:", str(inBuffer))
-    response = b''
-    while inBuffer > 0:
-        print("Reading serial port buffer.")
-        # response = response + ser.readline(inBuffer)
-        response = ser.readline(inBuffer)
-        import pdb; pdb.set_trace()
-        print("Response:", str(response))
-        time.sleep(sleep_time_after_buffer_read)
-        inBuffer = ser.inWaiting()
-        #print("Characters in receive buffer after reading and waiting %d seconds:" % sleep_time_after_buffer_read, str(ser.inWaiting()))
-    print("No more characters in serial port buffer.")
-    return response
-
-def exit_AT_mode(opt):
-    flush_ser()
-
-    command = b'\r\n'        # the ATO command must start on a newline
-    ser.write(command)
-    print('Sent command: (newline and return)', command.decode())
-    time.sleep(0.5)
-    if (opt == 'RT'):
-        command = b'ATO\r\n'     # exit AT command mode if we are in it
-    else:
-        command = b'RTO\r\n'
-    ser.write(command)
-    print('Sent command:', command.decode())
-    time.sleep(1)
-
-    print(get_response())
-
-## Write to file code
-def write2file (data_in):
-    data_in = data_in.decode()
-    write_data = ",\t".join([data_in[sum(write_len[:i]) : sum(write_len[:i+1])] for i in range(len(write_len))  ])
-    print(write_data)
-    f.write(str(write_data) + '\r\n')
-
-## =========================================
-
-if __name__ == '__main__':
-    ser = serial.Serial(
-        port='/dev/ttyS4',
-        baudrate=57600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout = 5
-    )
-    print('Opening serial port.') ## temp
-    #print(ser)
-
-    f = open(file_name, 'w+')
-    f.write('Time,\tGPS Coordinates,\tAltitude\r\n')
-    for x in range(0, 5):
-        write2file(gen_rand_bytes())
-
-    # test_baud()
-
-    enter_AT_mode()
-
     command = b'ATI\r\n'
     print(send_command(command))
 
     time.sleep(3)
-    cmds = ['ATS4=27', 'ATS4?', 'RTS4=27', 'RTS4?']
-#    cmd = '1'
+    cmds = ['ATS4=27', 'ATS4?']
     for item in cmds:
-#            command = input('Enter AT/RT command: ')
-        ## ie. ATSn? , register number n= 0 to 18
-        ## For example: use RTI,1 to get RTI value for node 1 or simply use
-        ## RTI to get the RTI value for the node set in the local NODEDESTINATION.
-        ## RTI[,x], RTI3[,x]
-#            command = command.encode()
-
-#            rt = send_command(command)
         rt = send_command(item.encode())
         print(rt)
         time.sleep(0.2)
@@ -157,31 +73,26 @@ if __name__ == '__main__':
         print(rt)
         time.sleep(0.2)
 
-#            cmd = input('Exit command mode? \r\n\t(integers only, 1 to stay, others to exit)')
-#            cmd = cmd.rstrip()
-
-    exit_AT_mode('RT')
-    time.sleep(0.1)
+#    exit_AT_mode('RT')
+#    time.sleep(0.1)
     exit_AT_mode('AT')
 
-    read_en = True;
+
+def loop():
+    inBuffer = ser.inWaiting()
+    response = ser.readline(inBuffer)
+
+#    response = ser.readline()
+    print(response)
+    time.sleep(SLEEP_INTERVAL/2)
+
+
+if __name__ == '__main__':
+    print("=== Setup ===")
+    setup()
+    print("=== Loop ===")
     while True:
         try:
-            rd = ser.readline()
-            import pdb; pdb.set_trace()
-            #rd = rd.decode()
-    #        rd = rd.zfill(total_len)
-
-            write2file(rd)
-            print(rd)
-
-            time.sleep(0.1)
-            if rd.rstrip() == b'':
-                read_en = False
-                time.sleep(0.5)
+            loop()
         except Exception as e:
-            print('Empty string')
-
-    ser.close()
-    f.close()
-    print('Closing serial port.') ## temp
+            print(e)
--- ground_station/radio/radio.ino
+++ ground_station/radio/radio.ino
@@ -1,46 +1,39 @@
 
-// Serial mySerial(0, 1);
-int ctr;
 
 void setup() {
   Serial.begin(57600);
-  ctr = 0;
-  delay(5000);
+
+  Serial.write("\r\n");
+  delay(1000);
+  
+  Serial.write("ATI\r\n");
+  delay(2000);
+  
+  Serial.write("+++");
+  delay(3000);
+
+  Serial.write("ATI\r\n");
+  delay(2000);
+
+  Serial.write("ATS4=27\r\n");
+  delay(1000);
+  
+  Serial.write("\r\n");
+  delay(2000);
+  
+  Serial.write("ATO\r\n");
+  delay(3000);
 }
 
-// Copy this part into main Arduino code
-// ==========================================================================================
-// Air data rate in one byte form, max value is 255
-// Current value: 64
-#define rx_air_speed 64
-byte radio_array[rx_air_speed];
-
-void radio_send ( byte data[rx_air_speed] ) {
-  for ( int i = 0; i < rx_air_speed; i++) {
-    Serial.write(data[i]);
-    delay(200);
-  }
-}
-// ==========================================================================================
 
 
 void loop() {
   
-  for (int i = 0; i < rx_air_speed; i++ ){
-    radio_array[i] = i+1;
-    radio_send(radio_array[i]);
-  }
-
-  if (Serial.available() && ctr < 20) {
-    if (ctr == -1) {
-        delay(500);
-        Serial.write("Hello, world!\r\n");
-        Serial.write("\t- From Tx\r\n");
-    }
-    Serial.write(ctr);
-    //Serial.write("\r\n");
-//    Serial.flush();
-    delay(1000);
-    ctr++;
-  }
+  Serial.write(0x48);   // H
+  Serial.write(0x45);   // E
+  Serial.write(0x4C);   // L
+  Serial.write(0x4C);   // L
+  Serial.write(0x4F);   // O
+  Serial.write("\r\n");
+  delay(2000);
 }
