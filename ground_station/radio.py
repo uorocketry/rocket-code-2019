361846e24ce1d07541cf32d5f8536d31061ce25d
azqlin <anthonyzqlin@gmail.com>
2019-06-08 14:59:58 -0400

    clean af

--- ground_station/radio.py
+++ ground_station/radio.py
@@ -46,36 +46,6 @@ def flush_ser():
 def setup():
     flush_ser()
 
-    command = b'\r\n'        # the ATO command must start on a newline
-    ser.write(command)
-    print("Sent command: ", command)
-
-    command = b'ATI\r\n'     # test to see if we are stuck in AT command mode.  If so, we see a response from this.
-    ser.write(command)
-    print('Sent command:', command.decode())
-    time.sleep(2)           # minimum 1 second wait needed before +++
-
-    command = b'+++'         # +++ enters AT command mode
-    ser.write(command)
-    print('Sent command:', command.decode())
-    time.sleep(3)           # minimum 1 second wait after +++
-
-    command = b'ATI\r\n'
-    print(send_command(command))
-
-    time.sleep(3)
-    cmds = ['ATS4=27', 'ATS4?', 'RTS4=27', 'RTS4?']
-    for item in cmds:
-        rt = send_command(item.encode())
-        print(rt)
-        time.sleep(0.2)
-        rt = send_command(b'\r\n')
-        print(rt)
-        time.sleep(0.2)
-    exit_AT_mode('RT')
-    time.sleep(0.1)
-    exit_AT_mode('AT')
-
 
 def loop():
 
